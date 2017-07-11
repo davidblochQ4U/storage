07006699996bd6eae8fcf3471b8824b55c293e05
davidblochQ4U <davidbloch03@gmail.com>
2017-07-11 23:43:23 +0200

    Structure remote method

--- common/models/container.js
+++ common/models/container.js
@@ -2,4 +2,29 @@
 
 module.exports = function(Container) {
 
+	Container.test = function() {
+        }
+    
+    Container.remoteMethod('test', { 
+
+        http: {
+            path: '/catchFile',
+            verb: 'post'
+        },
+        accepts: [{
+            arg: 'csvFile',
+            type: 'string',
+            required: true
+        }, {
+            arg: 'nomCabinet',
+            type: 'string',
+            required: false
+        }],
+        returns: {
+            arg: 'response',
+            type: 'string'
+        },
+        description: "Méthode test"       
+    });
+
 };
